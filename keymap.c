0a52aed896ac743116b6b858479ba0717b34c41e
Ale M <bomberstudios@gmail.com>
2021-08-26 12:26:35 +0200

    Cleanup

--- keymap.c
+++ keymap.c
@@ -12,33 +12,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_LCTRL,KC_LALT, KC_LCMD, L_NUM,   KC_BSPC, SPC_3,   L_SYM,   KC_RCMD, KC_RALT, KC_RCTRL,KC_ENT
   ),
   [_QWERTY] = LAYOUT(
-    _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,_______,
-    _______, CTL_A,   ALT_S,   CMD_D,   SHT_F,   KC_G,    KC_H,    SHT_J,   CMD_K,   ALT_L,   CTL_CLN ,_______,
-    _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,_______,
-    _______, KC_ESC,  KC_TAB,  KC_LGUI, L_NUM,   KC_BSPC, SPC_3,   L_SYM,   KC_MINS, KC_QUOT, KC_ENT,  _______
+    _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+    _______, CTL_A,   ALT_S,   CMD_D,   SHT_F,   KC_G,    KC_H,    SHT_J,   CMD_K,   ALT_L,   CTL_CLN, _______,
+    _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
   [_MOVE] = LAYOUT(
-    _______, KC_ESC,  L_COL,   CTL_TAB, CMD_TAB, LINEDEL, LINEUP,  START,   KC_UP,   END,     KC_BSPC,_______,
-    _______, Ctrl,    Alt,     Cmd,     Shift,   LINEDUP, LINEDWN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_ENT, _______,
-    _______, UNDO,    CUT,     COPY,    CPYPASTE,SKRUN,   SKRUNAG, SHT_SCR, SHT_ARE, SHT_OPT, _______,_______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,_______
+    _______, KC_ESC,  L_COL,   CTL_TAB, CMD_TAB, LINEDEL, LINEUP,  START,   KC_UP,   END,     KC_BSPC, _______,
+    _______, Ctrl,    Alt,     Cmd,     Shift,   LINEDUP, LINEDWN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_ENT,  _______,
+    _______, UNDO,    CUT,     COPY,    CPYPASTE,SKRUN,   SKRUNAG, SHT_SCR, SHT_ARE, SHT_OPT, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
   [_NUMBER] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_7,    KC_8,    KC_9,    _______,_______,
-    _______, CMD_1,   CMD_2,   CMD_3,   CMD_4,   CMD_5,   _______, KC_4,    KC_5,    KC_6,    KC_0,   _______,
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, KC_1,    KC_2,    KC_3,    KC_PENT,_______,
-    _______, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, _______,_______
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_7,    KC_8,    KC_9,    _______, _______,
+    _______, CMD_1,   CMD_2,   CMD_3,   CMD_4,   CMD_5,   _______, KC_4,    KC_5,    KC_6,    KC_0,    _______,
+    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, KC_1,    KC_2,    KC_3,    KC_PENT, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, _______, _______
   ),
   [_SYMBOL] = LAYOUT(
-    _______, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,_______,
-    _______, M_EMAIL, _______, KC_GRV,  KC_QUOT, KC_COLN, KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,_______,
-    _______, RESET,   _______, KC_TILD, KC_DQUO, KC_SCLN, KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,_______,
-    _______, _______, _______, _______, L_ADJ,   KC_DEL,  _______, _______, _______, _______, _______,_______
+    _______, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, M_EMAIL, _______, KC_GRV,  KC_QUOT, KC_COLN, KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
+    _______, RESET,   _______, KC_TILD, KC_DQUO, KC_SCLN, KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+    _______, _______, _______, _______, L_ADJ,   KC_DEL,  _______, _______, _______, _______, _______, _______
   ),
   [_ADJUST] = LAYOUT(
-    _______, QWERTY,  COLEMAK, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_VOLU,_______,
-    _______, M_EMAIL, _______, KC_GRV,  KC_QUOT, KC_COLN, KC_BSLS, KC_MPLY, KC_EQL,  KC_LBRC, KC_MUTE,_______,
-    _______, RESET,   _______, KC_TILD, KC_DQUO, KC_SCLN, KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_VOLD,_______,
-    _______, _______, _______, _______, L_ADJ,   KC_DEL,  _______, _______, _______, _______, _______,_______
+    _______, QWERTY,  COLEMAK, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_VOLU, _______,
+    _______, M_EMAIL, _______, KC_GRV,  KC_QUOT, KC_COLN, KC_BSLS, KC_MPLY, KC_EQL,  KC_LBRC, KC_MUTE, _______,
+    _______, RESET,   _______, KC_TILD, KC_DQUO, KC_SCLN, KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_VOLD, _______,
+    _______, _______, _______, _______, L_ADJ,   KC_DEL,  _______, _______, _______, _______, _______, _______
   )
 };
