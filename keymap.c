8a666242c673144c2d70a89254899e2cb591bea9
Ale M <bomberstudios@gmail.com>
2023-02-23 10:55:17 +0100

    Tweaks

--- keymap.c
+++ keymap.c
@@ -1,5 +1,4 @@
 // Cornelius
-
 #include QMK_KEYBOARD_H
 
 #include "../../../users/bomberstudios/keycodes.h"
@@ -9,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     ZOOMLEFT,CTL_A,   ALT_R,   CMD_S,   SHT_T,   KC_D,    KC_H,    SHT_N,   CMD_E,   ALT_I,   CTL_O,   ZOOMRIGHT,
     ZOOMFULL,KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ZOOMFULL,
-    KC_ESC,  KC_LCTL, KC_LALT, KC_LCMD, L_NUM,   KC_BSPC, SPC_3,   L_SYM,   KC_RCMD, KC_RALT, KC_RCTL, KC_ENT
+    EMOJI,   _______, _______, L_SYM,   L_NUM,   KC_BSPC, SPC_3,   L_SYM,   L_NUM,   _______, _______, KC_ENT
   ),
   [_QWERTY] = LAYOUT(
     _______, _______, _______, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
@@ -18,19 +17,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
   [_MOVE] = LAYOUT(
-    _______, KC_ESC,  L_COL,   CTL_TAB, CMD_TAB, LINEDEL, LINEUP,  START,   KC_UP,   END,     KC_BSPC, _______,
+    _______, _______, L_COL,   CTL_TAB, CMD_TAB, LINEDEL, LINEUP,  START,   KC_UP,   END,     _______, _______,
     _______, Ctrl,    Alt,     Cmd,     Shift,   LINEDUP, LINEDWN, KC_LEFT, KC_DOWN, KC_RIGHT,KC_ENT,  _______,
     _______, UNDO,    CUT,     COPY,    CPYPASTE,SKRUN,   SKRUNAG, SHT_SCR, SHT_ARE, SHT_OPT, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, M_PASS
   ),
   [_NUMBER] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_7,    KC_8,    KC_9,    _______, _______,
-    _______, CMD_1,   CMD_2,   CMD_3,   CMD_4,   CMD_5,   _______, KC_4,    KC_5,    KC_6,    KC_0,    _______,
+    CMD_0,   CMD_1,   CMD_2,   CMD_3,   CMD_4,   CMD_5,   _______, KC_4,    KC_5,    KC_6,    KC_0,    _______,
     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, KC_1,    KC_2,    KC_3,    KC_PENT, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, _______, _______
   ),
   [_SYMBOL] = LAYOUT(
-    _______, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
     _______, M_EMAIL, _______, KC_GRV,  KC_QUOT, KC_COLN, KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
     _______, QK_BOOT, _______, KC_TILD, KC_DQUO, KC_SCLN, KC_PIPE, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
     _______, _______, _______, _______, L_ADJ,   KC_DEL,  _______, _______, _______, _______, _______, _______
